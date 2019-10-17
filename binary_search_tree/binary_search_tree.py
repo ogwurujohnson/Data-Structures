f9d1b5cb6b36d56f6bb466b3b355ebb21bfd9e0d
ogwurujohnson <ogwurujohnson@gmail.com>
2019-10-17 08:10:38 +0100

    Implement the binary search tree insert, contain and get max algorithm

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
+from dll_stack import Stack
+from dll_queue import Queue
 import sys
 sys.path.append('../queue_and_stack')
-from dll_queue import Queue
-from dll_stack import Stack
 
 
 class BinarySearchTree:
@@ -12,16 +12,50 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if self.value is None:
+            self.value = BinarySearchTree(value)
+        else:
+            if value < self.value:
+                if self.left:
+                    self.left.insert(value)
+                else:
+                    self.left = BinarySearchTree(value)
+            else:
+                if self.right:
+                    self.right.insert(value)
+                else:
+                    self.right = BinarySearchTree(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if self.value == target:
+            return True
+        else:
+            if target < self.value and self.left:
+                return self.left.contains(target)
+            elif target > self.value and self.right:
+                return self.right.contains(target)
+        return False
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if self.value is None:
+            return None
+        if self.right is None:
+            return self.value
+        else:
+            return self.right.get_max()
+
+    # Call the function `cb` on the value of each node
+    # You may use a recursive or iterative approach
+
+    def for_each(self, cb):
+        cb(self.value)
+        if self.left is not None:
+            self.left.for_each(cb)
+        if self.right is not None:
+            self.right.for_each(cb)
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
